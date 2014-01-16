326fab2f505b801d05397ff7728ee64abee12700
Loïc Frering <loic.frering@gmail.com>
2014-01-17 00:50:33 +0100

    sed in place.

--- build/before.sh
+++ build/before.sh
@@ -5,6 +5,6 @@ set -e
 cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
-sed -e "s|%TRAVIS_BUILD_DIR%|$TRAVIS_BUILD_DIR|g" build/travis-php-nginx.conf
+sed -i -e "s|%TRAVIS_BUILD_DIR%|$TRAVIS_BUILD_DIR|g" build/travis-php-nginx.conf
 sudo cp build/travis-php-nginx.conf /etc/nginx/sites-available/default
 sudo service nginx restart
